e3d39f59ce6f54563e26f6aaa6ba9ba0dd1edace
Jasleen74 <preetjee@gmail.com>
2025-11-02 13:12:42 +0530

    ch 1 and 2

---
+++ ch-02/operators.py
@@ -0,0 +1,54 @@
+# arithmetic operators
+a = 7
+b = 17
+c = a+b # + is an arithmetic operator
+print("the value of a plus b is ",c)
+
+
+#Assignment opertors
+print('\n')
+print("Assignment Operators")
+a= 4-2 # it assins 4-2 in a
+b = 4
+b+=3 # increment of 3 in the value of b and then assign it to the b
+# if we write b-=3 then it will decrease the value of b by 3 and then assign it to the b
+#if b*=3 then it will multiply by three
+#if b/= then it will divide by three
+print("the new value of a is ",a)
+print("the new value of b is ",b)
+
+
+#Comparison operators
+print('\n')
+print("Comparison  Operators")
+d = 5<4      #this statement is false so it will give us "false"
+print(d)
+e = 5>4  #this statement is true so it will five us "true"
+print(e)
+f = 7>=7  # here >= is just one condition means greater than "OR" equal to means first is not true then other is
+print(f)
+
+
+#Logical Operators
+
+#Truth table for 'or'
+print('\n')
+print("Logical Operator  'or' ")
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+#Truth table for 'and'
+print('\n')
+print("Logical operator  'and' ")
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ",False and False)
+
+
+
+
+
+
---
+++ ch-02/variables.py
@@ -0,0 +1,20 @@
+a = 7   # a is a container to store a value
+b = 8
+# variable is a container
+# in our compuer's RAM under the name of the varibale a the value of 1 gets stored
+print(a+b)
+
+c = "mustang" # c can also be called a indentifier
+print(c)
+# " " when we write something in double quotes it is a string
+name = "harry"  # we can write idnetifier as this too or as a1 etc
+print(name)
+
+d = 7.7 # 7.7 is a floating point number
+print(d)
+
+e = False # this is a boolean variable ,thisis only for yes or no :-)
+
+g = None # g is a none tyep variable
+# 1sam is not a way to define a varibale .
+# it should not start with  @sam or s@m cuz @ is a special charcater
